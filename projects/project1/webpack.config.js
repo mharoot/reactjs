f03290ec9e2bee2b44a6242fe1d254c6a8af50e4
Michael Harootoonyan <michaelharootoonyan@yahoo.com>
2017-07-29 20:22:30 -0700

    modified fixed errors

--- projects/project1/webpack.config.js
+++ projects/project1/webpack.config.js
@@ -3,7 +3,7 @@ var config = {
    entry: './main.js',
 	
    output: {
-      path:'./',
+      path:'/.',
       filename: 'index.js',
    },
 	
@@ -17,7 +17,7 @@ var config = {
          {
             test: /\.jsx?$/,
             exclude: /node_modules/,
-            loader: 'babel',
+            loader: 'babel-loader',
 				
             query: {
                presets: ['es2015', 'react']
